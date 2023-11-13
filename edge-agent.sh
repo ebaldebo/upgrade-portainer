92533978abb23a138783bb77235317ba3760903b
Baldebo, Emil <ebaldebo@gmail.com>
2023-11-13 09:36:04 +0100

    edge-agent.sh

---
+++ edge-agent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+docker stop portainer_edge_agent
+docker rm portainer_edge_agent
+
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /var/lib/docker/volumes:/var/lib/docker/volumes  \
+    -v /:/host -v portainer_agent_data:/data \
+    --restart always \
+    -e EDGE=1 \
+    -e EDGE_ID=$PORTAINER_EDGE_ID \
+    -e EDGE_KEY=$PORTAINER_EDGE_KEY \
+    -e EDGE_INSECURE_POLL=1 \
+    --name portainer_edge_agent portainer/agent:latest
